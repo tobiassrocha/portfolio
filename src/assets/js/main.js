ca914500d38ae2f948831688b4761645cd5ea682
Tobias da Rosa <tobias.rosa@compasso.com.br>
2020-01-13 12:31:00 -0300

    section skills done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.content__skills-circle').circleProgress({
+        animation: false,
         size: 150,
         fill: {gradient: ["#54B689", "#69e0aa"]}
     });
