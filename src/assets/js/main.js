38f14164ae24246307312113f2bac1daa49f7404
Tobias da Rosa <tobias.rosa@compasso.com.br>
2020-01-14 11:36:13 -0300

    add navigation for anchor

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         size: 150,
         fill: {gradient: ["#54B689", "#69e0aa"]}
     });
+
+    $('.header__navbar a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),targetOffset = $(id).offset().top;
+                
+        $('html, body').animate({ 
+            scrollTop: targetOffset
+        }, 1000);
+    });
 });
